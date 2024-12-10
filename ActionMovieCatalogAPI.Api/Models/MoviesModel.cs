c6399355cf869a87b9c9f7d0330a08f5d68b64e3
KeoCoelho <keoxcoelho@gmail.com>
2024-12-10 17:38:13 -0300

    chore: update

--- ActionMovieCatalogAPI.Api/Models/MoviesModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ActionMovieCatalogAPI.Api.Models;
-
-public class Movie
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-
-}
--- ActionMovieCatalogAPI.Api/Models/ReviewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ActionMovieCatalogAPI.Api.Models;
-
-public class Review
-{
-    public int Id { get; set; }
-    public string Content { get; set; } = string.Empty;
-    public int MovieId { get; set; } 
-    public Movie Movie { get; set; } = new Movie();
-}
