e4bbb4afb8c5b1ae08a60d2cbdc0d5846ed84351
Keo Coelho <keoxcoelho@gmail.com>
2024-12-18 00:30:23 -0300

    update

--- src/ActionMoviesCatalog.Api/Models/MovieModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ActionMoviesCatalog.Api.Models;
-public class Movie
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-        
-    public Year { get; set; } = 0;
-    
-}
