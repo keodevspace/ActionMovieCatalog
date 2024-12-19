e2c67959ea56e6b175662bbbc333ef4c15b87804
Keo Coelho <keoxcoelho@gmail.com>
2024-12-19 08:30:56 -0300

    fix: Git bug solved

--- ActionMoviesCatalog.Api/Entities/Movie.cs
+++
@@ -1,7 +0,0 @@
-namespace ActionMoviesCatalog.Api.Entities;
-public class Movie
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-}
